982e08e2a1dc05f95a1c3187d7d0fc47f8f39443
Andrew Smith <andrew.smith@paymark.co.nz>
2015-06-15 00:35:00 +0000

    Update to use UUID length

--- src/config/sql/memberDB.sql
+++ src/config/sql/memberDB.sql
@@ -1,9 +1,9 @@
 CREATE SCHEMA if not exists member;
 CREATE TABLE if not exists member.member(
-	id CHAR(100),
+	id CHAR(36),
 	role CHAR(100) NOT NULL,
 	`status` CHAR(100) NOT NULL,
-	organisationId CHAR(100) NOT NULL,
-	userId CHAR(100),
+	organisationId CHAR(36) NOT NULL,
+	userId CHAR(36),
 	PRIMARY KEY (id)
 );
