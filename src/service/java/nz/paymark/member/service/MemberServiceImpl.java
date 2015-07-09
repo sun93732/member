ce0bcf799877d497e8524d64c7d0e99a2e2ff712
Rohit Advani <rohit.advani@clearpoint.co.nz>
2015-07-10 10:55:40 +1200

    Loggers added in MemberServiceImpl while throwing exception in updateMember method

--- src/service/java/nz/paymark/member/service/MemberServiceImpl.java
+++ src/service/java/nz/paymark/member/service/MemberServiceImpl.java
@@ -15,120 +15,146 @@ import nz.paymark.member.api.MemberService;
 import nz.paymark.member.dao.MemberDao;
 import nz.paymark.member.model.Member;
 import nz.paymark.member.model.MemberSearchCriteria;
-
+import org.slf4j.Logger;
+import nz.paymark.logging.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
 /***
  *
  * @author rohitadvani
- *@description Member Service Implementation for handling different operations for member object
+ * @description Member Service Implementation for handling different operations
+ *              for member object
  */
 
 @Component
 public class MemberServiceImpl implements MemberService {
-	
-	@Resource
-	MemberDao memberDao;
+    
+    private static final Logger logger = LoggerFactory.getLogger();
 
-	/**
-	 * 
-	 * @param member - Member object containing attributes (ORG_ID, USER_ID, ROLE ETC.)
-	 * @return The member that was created or exception if the member is not created.
-	 */
-	
-	@Override
-	public Member createMember(Member member) {
-		throwBadRequestIf(member.getUserId() == null, "userId", "UserId (UUID) should not be null.");
-		throwBadRequestIf(member.getOrganisationId() == null, "organisationId", "OrganisationId (UUID) should not be null.");
-		throwBadRequestIf(member.getRole() == null, "role", "Member role should not be null.");
-		try{
-			Member newMember = memberDao.createMember(member);
-			return newMember;
-		}catch(DataIntegrityViolationException ex){
-			throw new ConflictException("Combination of UserId and OrganisationId must be unique");
-		}
-		
-	}
-	
-	/**
-	 * 
-	 * @param memberId - Member Id of the member that needs to be updated
-	 * @param member - Member that needs to be updated
-	 * @return The member that was updated or exception if the member is not updated.
-	 */
+    @Resource
+    MemberDao memberDao;
 
-	@Override
-	public Member updateMember(Member member) {
-		throwBadRequestIf(member.getId() == null, "id", "Id should not be null.");
-		throwBadRequestIf(member.getUserId() == null, "userId", "UserId (UUID) should not be null.");
-		throwBadRequestIf(member.getOrganisationId() == null, "organisationId", "OrganisationId (UUID) should not be null.");
-		throwBadRequestIf(member.getRole() == null, "role", "Member role should not be null.");
-		Member dbMember = memberDao.getMember(member.getId());
-		Member updatedMember = new Member();
-		if (dbMember == null) {
-			throw new RecordNotFoundException();
-		}
-		
-		if (!dbMember.getCreationTime().equals(member.getCreationTime())) {
-	           throw new ForbiddenException("Creation time cannot be modified.");
-	    }
-		try{
-			updatedMember = memberDao.updateMember(member);
-		}catch(OptimisticLockException e){
-			throw new ValidationException("Modified time is inconsistent in the request. Please perform fresh GET before PUT request.");
-		}
-		return updatedMember;
-	}
-	
-	/**
-	 * 
-	 * @param memberId - Member Id of the member that needs to be retrieved
-	 * @return The retrieved member
-	 */
+    /**
+     * 
+     * @param member
+     *            - Member object containing attributes (ORG_ID, USER_ID, ROLE
+     *            ETC.)
+     * @return The member that was created or exception if the member is not
+     *         created.
+     */
 
-	@Override
-	public Member getMember(String id) throws RecordNotFoundException {
-		throwBadRequestIf(id == null, "id", "Id should not be null.");
-		throwBadRequestIf(id.length() == 0 , "id", "Id should not be null.");
-		Member member = memberDao.getMember(id);
+    @Override
+    public Member createMember(Member member) {
+        throwBadRequestIf(member.getUserId() == null, "userId",
+                "UserId (UUID) should not be null.");
+        throwBadRequestIf(member.getOrganisationId() == null, "organisationId",
+                "OrganisationId (UUID) should not be null.");
+        throwBadRequestIf(member.getRole() == null, "role",
+                "Member role should not be null.");
+        try {
+            Member newMember = memberDao.createMember(member);
+            return newMember;
+        } catch (DataIntegrityViolationException ex) {
+            throw new ConflictException(
+                    "Combination of UserId and OrganisationId must be unique");
+        }
+
+    }
+
+    /**
+     * 
+     * @param memberId
+     *            - Member Id of the member that needs to be updated
+     * @param member
+     *            - Member that needs to be updated
+     * @return The member that was updated or exception if the member is not
+     *         updated.
+     */
+
+    @Override
+    public Member updateMember(Member member) {
+        throwBadRequestIf(member.getId() == null, "id",
+                "Id should not be null.");
+        throwBadRequestIf(member.getUserId() == null, "userId",
+                "UserId (UUID) should not be null.");
+        throwBadRequestIf(member.getOrganisationId() == null, "organisationId",
+                "OrganisationId (UUID) should not be null.");
+        throwBadRequestIf(member.getRole() == null, "role",
+                "Member role should not be null.");
+        Member dbMember = memberDao.getMember(member.getId());
+        Member updatedMember = new Member();
+        if (dbMember == null) {
+            throw new RecordNotFoundException();
+        }
+
+        if (!dbMember.getCreationTime().equals(member.getCreationTime())) {
+            logger.info("updateMember : Updated failed due to creation time's not matching with database for Member UUID: "
+                    + member.getId());
+            throw new ForbiddenException("Creation time cannot be modified.");
+        }
+        try {
+            updatedMember = memberDao.updateMember(member);
+        } catch (OptimisticLockException e) {
+            logger.info("updateMember : Updated failed due to modification time's not matching with database for Member UUID: "
+                    + member.getId());
+            throw new ValidationException(
+                    "Modified time is inconsistent in the request. Please perform fresh GET before PUT request.");
+        }
+        return updatedMember;
+    }
+
+    /**
+     * 
+     * @param memberId
+     *            - Member Id of the member that needs to be retrieved
+     * @return The retrieved member
+     */
+
+    @Override
+    public Member getMember(String id) throws RecordNotFoundException {
+        throwBadRequestIf(id == null, "id", "Id should not be null.");
+        throwBadRequestIf(id.length() == 0, "id", "Id should not be null.");
+        Member member = memberDao.getMember(id);
         if (member == null)
             throw new RecordNotFoundException();
         return member;
-	}
-	
-	/**
-	 * Returns a list of the Members found by the search
-	 * 
-	 * @param ids A list of IDs to search for
-	 * @return A list of Members
-	 */
+    }
 
-	@Override
-	public List<Member> searchMembers(List<String> ids) {
-		return memberDao.searchMembers(ids);
-	}
-	
-	/**
-	 * Returns a list of the Members found by the search
-	 * 
-	 * @param criteria A MemberSearchCriteria instance
-	 * @return A list of Members
-	 */
-	
-	@Override
-    public List<Member> searchMembersByCriteria(
-            MemberSearchCriteria criteria) {
+    /**
+     * Returns a list of the Members found by the search
+     * 
+     * @param ids
+     *            A list of IDs to search for
+     * @return A list of Members
+     */
+
+    @Override
+    public List<Member> searchMembers(List<String> ids) {
+        return memberDao.searchMembers(ids);
+    }
+
+    /**
+     * Returns a list of the Members found by the search
+     * 
+     * @param criteria
+     *            A MemberSearchCriteria instance
+     * @return A list of Members
+     */
+
+    @Override
+    public List<Member> searchMembersByCriteria(MemberSearchCriteria criteria) {
         return memberDao.searchMembersByCriteria(criteria);
     }
-	
-	/**
-	 * 
-	 * @param memberId - Member Id of the member that needs to be deleted
-	 */
 
-	@Override
-	public void deleteMember(String id) {
-		memberDao.deleteMember(id);
-	}
+    /**
+     * 
+     * @param memberId
+     *            - Member Id of the member that needs to be deleted
+     */
+
+    @Override
+    public void deleteMember(String id) {
+        memberDao.deleteMember(id);
+    }
 }
